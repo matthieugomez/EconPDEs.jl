f52e507e8296841e2bb733021373310d487f7288
matthieugomez <gomez.matthieu@gmail.com>
2019-07-04 18:46:00 -0400

    Update Haddad.jl

--- examples/AssetPricing/Haddad.jl
+++ examples/AssetPricing/Haddad.jl
@@ -27,14 +27,14 @@ function initialize_stategrid(m::HaddadModel; μn = 30, vn = 30)
   μbar = m.μbar; vbar = m.vbar ; κμ = m.κμ ; νμ = m.νμ ; κv = m.κv ; νv = m.νv ; αbar = m.αbar ; λ = m.λ ; ρ = m.ρ ; γ = m.γ ; ψ = m.ψ
 
   σ = sqrt(νμ^2 * vbar / (2 * κμ))
-  μmin = quantile(Normal(μbar, σ), 0.025)
-  μmax = quantile(Normal(μbar, σ), 0.975)
+  μmin = quantile(Normal(μbar, σ), 0.001)
+  μmax = quantile(Normal(μbar, σ), 0.999)
   μs = range(μmin, stop = μmax, length = μn)
 
   α = 2 * κv * vbar / νv^2
   β = νv^2 / (2 * κv)
-  vmin = quantile(Gamma(α, β), 0.025)
-  vmax = quantile(Gamma(α, β), 0.975)
+  vmin = quantile(Gamma(α, β), 0.001)
+  vmax = quantile(Gamma(α, β), 0.999)
   vs = range(vmin, stop = vmax, length = vn)
 
   OrderedDict(:μ => μs, :v => vs)
@@ -58,6 +58,7 @@ function (m::HaddadModel)(state::NamedTuple, y::NamedTuple)
   σv = νv * sqrt(v) 
   σp_Zμ = pμ / p * σμ
   σp_Zv = pv / p * σv
+  σp2 = σp_Zμ^2 + σp_Zv^2
   μp = pμ / p * μμ + pv / p * μv + 0.5 * pμμ / p * σμ^2 + 0.5 * pvv / p * σv^2
 
   # Market Price of Risk κ
@@ -66,18 +67,17 @@ function (m::HaddadModel)(state::NamedTuple, y::NamedTuple)
   κ_Zc = αstar * γ * σc
   κ_Zμ = (αstar - (1 / γ - 1) / (ψ - 1)) * γ * σp_Zμ
   κ_Zv = (αstar - (1 / γ - 1) / (ψ - 1)) * γ * σp_Zv
+  κ2 = κ_Zc^2 + κ_Zμ^2 + κ_Zv^2
 
   # Interest rate r
-  κ2 = κ_Zc^2 + κ_Zμ^2 + κ_Zv^2
-  σp2 = σp_Zμ^2 + σp_Zv^2
-  r = ρ + μc / ψ - (1 - 1 / ψ) / (2 * γ) * κ2 - (1 / ψ - γ) / (2 * γ * (ψ - 1)) * σp2 - (1 - γ) / (γ * ψ) * (κμ * σμ + κv * σv) + 1 / ψ  * (κ_Zc * σc + κμ * σμ + κv * σv)
+  #r = ρ + μc / ψ - (1 - 1 / ψ) / (2 * γ) * κ2 - (1 / ψ - γ) / (2 * γ * (ψ - 1)) * σp2 - (1 - γ) / (γ * ψ) * (κμ * σμ + κv * σv) + 1 / ψ  * (κ_Zc * σc + κμ * σμ + κv * σv)
 
   # Market pricing
-  out = p * (1 / p + μc + μp - r - κ_Zc * σc - κ_Zμ * σp_Zμ - κ_Zv * σp_Zv)
+  #out = p * (1 / p + μc + μp - r - κ_Zc * σc - κ_Zμ * σp_Zμ - κ_Zv * σp_Zv)
 
-  #out = p * (1 / p - ρ + (1 - 1 / ψ) * (μc - 0.5 * γ * σc^2 * (1 - (αstar - 1)^2)) + μp + (0.5 * (1 / ψ - γ) / (1 - 1 / ψ) + 0.5 * γ * (1 - 1 / ψ) * (αstar - 1)^2) * σp2)
+  out = p * (1 / p - ρ + (1 - 1 / ψ) * (μc - 0.5 * γ * σc^2 * (1 - (αstar - 1)^2)) + μp + (0.5 * (1 / ψ - γ) / (1 - 1 / ψ) + 0.5 * γ * (1 - 1 / ψ) * (αstar - 1)^2) * σp2)
 
-  return (out,) , (μμ, μv), (p = p, r = r, κ_Zc = κ_Zc, κ_Zμ = κ_Zμ, κ_Zv = κ_Zv, αstar = αstar)
+  return (out,) , (μμ, μv), (p = p, κ_Zc = κ_Zc, κ_Zμ = κ_Zμ, κ_Zv = κ_Zv, αstar = αstar)
 end
 
 m = HaddadModel()

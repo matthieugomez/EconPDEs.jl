05dfddd03949eee2b79a22fd07fd7118011ee991
matthieugomez <gomez.matthieu@gmail.com>
2019-09-19 12:56:56 -0400

    remove generated StateGrid

--- examples/ConsumptionProblem/WangWangYang.jl
+++ examples/ConsumptionProblem/WangWangYang.jl
@@ -10,12 +10,12 @@ struct WangWangYangModel
     wmax::Float64
 end
 
-function WangWangYangModel(;μ = 0.015, σ = 0.1, r = 0.035, ρ = 0.04, γ = 3, ψ = 1.1, wmax = 1000.0)
+function WangWangYangModel(;μ = 0.015, σ = 0.1, r = 0.035, ρ = 0.04, γ = 3, ψ = 1.1, wmax = 5000.0)
     WangWangYangModel(μ, σ, r, ρ, γ, ψ, wmax)
 end
 
 function initialize_stategrid(m::WangWangYangModel; n = 500)
-    OrderedDict(:w => range(0.0, stop = m.wmax, length = n))
+    OrderedDict(:w => range(0.0, stop = sqrt(m.wmax), length = n).^2)
 end
 
 function initialize_y(m::WangWangYangModel, stategrid)
@@ -23,28 +23,18 @@ function initialize_y(m::WangWangYangModel, stategrid)
 end
     
 function (m::WangWangYangModel)(state::NamedTuple, y::NamedTuple)
-    μ = m.μ ;  σ = m.σ ;  r = m.r ;  ρ = m.ρ ;  γ = m.γ ;  ψ = m.ψ  ; wmax = m.wmax
+    μ = m.μ ;  σ = m.σ ;  r = m.r ;  ρ = m.ρ ;  γ = m.γ ;  ψ = m.ψ ; wmax = m.wmax
     w = state.w
     p, pw, pww = y.p, y.pw, y.pww
     c = (r + ψ * (ρ - r)) * p * pw^(-ψ)
     μw = (r - μ + σ^2) * w + 1 - c
-    # This branch turns out to be unecessary. This is because, at w = 0, the PDE does not depend on the second derive (σ^2 * w^2 / 2= 0). Therefore, at w = 0, the PDE is simple a relationship between function and first derivative: this is the boundary counstraint.
+    # One only needs a ghost node if μw <= 0 (since w^2p_ww = 0). In this case, we obtain a formula for pw so that c <= 1
     if w ≈ 0.0 && μw <= 0.0
        pw = ((r + ψ * (ρ - r)) * p)^(1 / ψ)
        c = (r + ψ * (ρ - r)) * p * pw^(-ψ)
        μw = (r - μ + σ^2) * w + 1 - c
     end
-    # this branch is unnecessary when individuals dissave at the top (default)
-    if w ≈ wmax && μw >= 0.0
-        pw = 1.0
-        pww = 0.0
-        c = (r + ψ * (ρ - r)) * p * pw^(-ψ)
-        μw = (r - μ + σ^2) * w + 1 - c
-    end
-    # Since first derivative is upwinded, I can directly impose value of second derivative
-    if w ≈ wmax
-        pww = 0.0
-    end
+    # At the top, I use the solution of the unconstrainted, i.e. pw = 1 (I could also do reflecting boundary but less elegant)
     pt = (((r + ψ * (ρ - r)) * pw^(1 - ψ) - ψ * ρ) / (ψ - 1) + μ - γ * σ^2 / 2) * p + ((r - μ + γ * σ^2) * w + 1) * pw + σ^2 * w^2 / 2  * (pww - γ * pw^2 / p)
     μw = 1 + (r - μ + σ^2) * w - c
     return (pt,), (μw,), (w = w, p = p, pw = pw, pww = pww, μw = μw, c = c)
@@ -53,4 +43,4 @@ end
 m = WangWangYangModel()
 stategrid = initialize_stategrid(m)
 y0 = initialize_y(m, stategrid)
-y, result, distance0 = pdesolve(m, stategrid, y0)
+y, result, distance = pdesolve(m, stategrid, y0; bc = OrderedDict(:pw => (1.0, 1.0)))
--- src/pdesolve.jl
+++ src/pdesolve.jl
@@ -3,19 +3,16 @@
 Type State Grid
 
 ========================================================================================#
-struct StateGrid{N, V}
-    x::NamedTuple{N, V}
+struct StateGrid{T, N, C <: NamedTuple} <: AbstractArray{T, N}
+    x::C
 end
+StateGrid(x::NamedTuple{Names, <: NTuple{N, <: AbstractVector{T}}}) where {Names, N, T} = StateGrid{T, N, typeof(x)}(x)
 StateGrid(x::OrderedDict) = StateGrid((;x...))
-Base.size(stategrid::StateGrid) = map(length, values(stategrid.x))
-Base.ndims(stategrid::StateGrid{N, <: NTuple{K, R}}) where {N, K, R} = K
+Base.eltype(stategrid::StateGrid{T, N, <: NamedTuple{Names, V}}) where {T, N, Names, V} = NamedTuple{Names, NTuple{N, T}}
+Base.ndims(stategrid::StateGrid{T, N}) where {T, N} = N
+Base.size(stategrid::StateGrid{T, N}) where {T, N} = ntuple(i -> length(stategrid.x[i]), N)
 Base.eachindex(stategrid::StateGrid) = CartesianIndices(size(stategrid))
-@generated function Base.getindex(stategrid::StateGrid{N, <: NTuple{K, R}}, args::CartesianIndex) where {N, K, R}
-    quote
-        $(Expr(:meta, :inline))
-        $(Expr(:tuple, [Expr(:(=), N[i], :(stategrid.x[$i][args[$i]])) for i in 1:K]...))
-    end
-end
+Base.getindex(stategrid::StateGrid, args::CartesianIndex) = eltype(stategrid)(ntuple(i -> stategrid.x[i][args[i]], ndims(stategrid)))
 Base.getindex(stategrid::StateGrid, x::Symbol) = stategrid.x[x]
 
 #========================================================================================
@@ -24,7 +21,7 @@ Derive
 
 ========================================================================================#
 # 1 state variable
-@generated function derive(::Type{Tsolution}, grid::StateGrid{N, <: NTuple{1, R}}, y::AbstractArray{T}, icar, bc, drift = (0.0,)) where {Tsolution, N, R, T}
+@generated function derive(::Type{Tsolution}, grid::StateGrid{T1, 1, <: NamedTuple{N}}, y::AbstractArray{T}, icar, bc, drift = (0.0,)) where {Tsolution, T1, N, T}
     statename = N[1]
     expr = Expr[]
     for k in 1:length(Tsolution.parameters[1])
@@ -33,21 +30,20 @@ Derive
         push!(expr, Expr(:(=), Symbol(solname, statename), :((μx >= 0.0) ? ((i < size(y, 1)) ? (y[i+1, $k] - y[i, $k]) / Δxp : convert($T, bc[end, $k])) : ((i > 1) ? (y[i, $k] - y[i-1, $k]) / Δxm : convert($T, bc[1, $k])))))
         push!(expr, Expr(:(=), Symbol(solname, statename, statename), :((1 < i < size(y, 1)) ? (y[i + 1, $k] / (Δxp * Δx) + y[i - 1, $k] / (Δxm * Δx) - 2 * y[i, $k] / (Δxp * Δxm)) : ((i == 1) ? (y[2, $k] / (Δxp * Δx) + (y[1, $k] - bc[1, $k] * Δxm) / (Δxm * Δx) - 2 * y[1, $k] / (Δxp * Δxm)) : ((y[end, $k] + bc[end, $k] * Δxp) / (Δxp * Δx) + y[end - 1, $k] / (Δxm * Δx) - 2 * y[end, $k] / (Δxp * Δxm))))))
     end
-    out = Expr(:tuple, expr...)
     quote
         $(Expr(:meta, :inline))
         i = icar[1]
         μx = drift[1]
         grida = grid.x[1]
-        Δxm = grida[max(i, 2)] - grida[max(i-1, 1)]
-        Δxp = grida[min(i+1, size(y, 1))] - grida[min(i, size(y, 1) - 1)]
+        @inbounds Δxm = grida[max(i, 2)] - grida[max(i-1, 1)]
+        @inbounds Δxp = grida[min(i+1, size(y, 1))] - grida[min(i, size(y, 1) - 1)]
         Δx = (Δxm + Δxp) / 2
-        $out
+        @inbounds $(Expr(:tuple, expr...))
     end
 end
 
 # 2 state variables
-@generated function derive(::Type{Tsolution}, grid::StateGrid{N, <: NTuple{2, R}}, y::AbstractArray{T}, icar, bc, drift = (0.0, 0.0)) where {Tsolution, N, R, T}
+@generated function derive(::Type{Tsolution}, grid::StateGrid{T1, 2, <: NamedTuple{N}}, y::AbstractArray{T}, icar, bc, drift = (0.0, 0.0)) where {Tsolution, T1, N, T}
     statename1 = N[1]
     statename2 = N[2]
     expr = Expr[]
@@ -60,19 +56,18 @@ end
         push!(expr, Expr(:(=), Symbol(solname, statename2, statename2), :((1 < i2 < size(y, 2)) ? (y[i1, i2 + 1, $k] / (Δx2p * Δx2) + y[i1, i2 - 1, $k] / (Δx2m * Δx2) - 2 * y[i1, i2, $k] / (Δx2p * Δx2m)) : ((i2 == 1) ? (y[i1, 2, $k] / (Δx2p * Δx2) + (y[i1, 1, $k] - bc[i1, 1, $k] * Δx2m) / (Δx2m * Δx2) - 2 * y[i1, 1, $k] / (Δx2p * Δx2m)) : ((y[i1, end, $k] + bc[i1, end, $k] * Δx2p) / (Δx2p * Δx2) + y[i1, end - 1, $k] / (Δx2m * Δx2) - 2 * y[i1, end, $k] / (Δx2p * Δx2m))))))
         push!(expr, Expr(:(=), Symbol(solname, statename1, statename2), :((y[min(i1 + 1, size(y, 1)), min(i2 + 1, size(y, 2)), $k] - y[min(i1 + 1, size(y, 1)), max(i2 - 1, 1), $k] - y[max(i1 - 1, 1), min(i2 + 1, size(y, 2)), $k] + y[max(i1 - 1, 1), max(i2 - 1, 1), $k]) / (4 * Δx1 * Δx2))))
     end
-    out = Expr(:tuple, expr...)
     quote
         $(Expr(:meta, :inline))
         i1, i2 = icar[1], icar[2]
         μx1, μx2 = drift[1], drift[2]
         grid1, grid2 = grid.x[1], grid.x[2]
-        Δx1m = grid1[max(i1, 2)] - grid1[max(i1-1, 1)]
-        Δx1p = grid1[min(i1+1, size(y, 1))] - grid1[min(i1, size(y, 1) - 1)]
+        @inbounds Δx1m = grid1[max(i1, 2)] - grid1[max(i1-1, 1)]
+        @inbounds Δx1p = grid1[min(i1+1, size(y, 1))] - grid1[min(i1, size(y, 1) - 1)]
         Δx1 = (Δx1m + Δx1p) / 2
-        Δx2m = grid2[max(i2, 2)] - grid2[max(i2-1, 1)]
-        Δx2p = grid2[min(i2+1, size(y, 2))] - grid2[min(i2, size(y, 2) - 1)]
+        @inbounds Δx2m = grid2[max(i2, 2)] - grid2[max(i2-1, 1)]
+        @inbounds Δx2p = grid2[min(i2+1, size(y, 2))] - grid2[min(i2, size(y, 2) - 1)]
         Δx2 = (Δx2m + Δx2p) / 2
-        $out
+        @inbounds $(Expr(:tuple, expr...))
     end
 end
 
@@ -242,36 +237,32 @@ function _setindex!(a::OrderedDict, iτ::Integer, apm, stategrid::StateGrid, Tso
 ========================================================================================#
 
  function pdesolve(apm, grid::OrderedDict, y0::OrderedDict; is_algebraic = OrderedDict(k => false for k in keys(y0)), bc = nothing, kwargs...)
-     Tsolution = Type{tuple(keys(y0)...)}
-     stategrid = StateGrid(grid)
-     all(length.(values(y0)) .== prod(size(stategrid))) || throw("The length of initial solution does not equal the length of the state space")
-     is_algebraic = OrderedDict(k => fill(is_algebraic[k], size(y0[k])) for k in keys(y0))
-     y = OrderedDict{Symbol, Array{Float64, ndims(stategrid)}}(x =>  Array{Float64}(undef, size(stategrid)) for x in keys(y0))
-
-     # Convert to Matrix
-     y0_M = _Array(y0)
-     ysize = size(y0_M)
-     is_algebraic_M = _Array(is_algebraic)
-     bc_M = _Array_bc(bc, y0_M, y0, grid)
-
-     # prepare dict
-     a_keys = get_keys(apm, stategrid, Tsolution, y0_M, bc_M)
-     a = nothing
-     if a_keys !== nothing
-        a = OrderedDict{Symbol, Array{Float64, ndims(stategrid)}}(a_key => Array{Float64}(undef, size(stategrid)) for a_key in a_keys)
-     end
-
-     # create sparsity
-     J0c = sparsity_jac(stategrid, y0)
-
-     y_M, distance = finiteschemesolve((ydot, y) -> hjb!(apm, stategrid, Tsolution, ydot, y, bc_M, ysize), vec(y0_M); is_algebraic = vec(is_algebraic_M),  J0c = J0c, kwargs... )
-     y_M = reshape(y_M, ysize...)
-     _setindex!(y, 1, y_M)
-     if a_keys !== nothing
-         _setindex!(a, 1, apm, stategrid, Tsolution, y_M, bc_M)
-         a = merge(y, a)
-     end
-     return y, a, distance
+    Tsolution = Type{tuple(keys(y0)...)}
+    stategrid = StateGrid(grid)
+    all(length.(values(y0)) .== prod(size(stategrid))) || throw("The length of initial solution does not equal the length of the state space")
+    is_algebraic = OrderedDict(k => fill(is_algebraic[k], size(y0[k])) for k in keys(y0))
+    y = OrderedDict{Symbol, Array{Float64, ndims(stategrid)}}(x =>  Array{Float64}(undef, size(stategrid)) for x in keys(y0))
+    # Convert to Matrix
+    y0_M = _Array(y0)
+    ysize = size(y0_M)
+    is_algebraic_M = _Array(is_algebraic)
+    bc_M = _Array_bc(bc, y0_M, y0, grid)
+    # prepare dict
+    a_keys = get_keys(apm, stategrid, Tsolution, y0_M, bc_M)
+    a = nothing
+    if a_keys !== nothing
+       a = OrderedDict{Symbol, Array{Float64, ndims(stategrid)}}(a_key => Array{Float64}(undef, size(stategrid)) for a_key in a_keys)
+    end
+    # create sparsity
+    J0c = sparsity_jac(stategrid, y0)
+    y_M, distance = finiteschemesolve((ydot, y) -> hjb!(apm, stategrid, Tsolution, ydot, y, bc_M, ysize), vec(y0_M); is_algebraic = vec(is_algebraic_M),  J0c = J0c, kwargs... )
+    y_M = reshape(y_M, ysize...)
+    _setindex!(y, 1, y_M)
+    if a_keys !== nothing
+        _setindex!(a, 1, apm, stategrid, Tsolution, y_M, bc_M)
+        a = merge(y, a)
+    end
+    return y, a, distance
  end
 
 

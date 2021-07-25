9d62c3bd3fbe459049a5b35558cc684c9c357846
Matthieu Gomez <gomez.matthieu@gmail.com>
2021-07-25 09:51:34 -0700

    Update Leland.jl

--- examples/OptimalStoppingTime/Leland.jl
+++ examples/OptimalStoppingTime/Leland.jl
@@ -1,54 +1,27 @@
 using EconPDEs
 
-struct LelandModel{T<:Real}
+struct LelandModel
     # consumption process parameters
-    r::T
-    μ::T
-    σ::T
-    C::T # coupon rate
-    τ::T # tax rate
-    # parameters for analytical solution
-    Δ::T
-    γ::T
-    η::T
-    δb::T
+    r::Float64
+    μ::Float64
+    σ::Float64
+    C::Float64 # coupon rate
+    τ::Float64 # tax rate
 end
 
-function LelandModel(; 
-    r = 0.02, 
-    μ = 0.0, 
-    σ = 0.1, 
-    C = 0.05, 
-    τ = 0.2, 
-    Δ = (1/2 * σ^2 - μ)^2 + 2 * σ^2 * r,
-    γ = (μ - 1/2 * σ^2 + sqrt(Δ))/σ^2,
-    η = -(μ - 1/2 * σ^2 - sqrt(Δ))/σ^2,
-    δb = (1-τ) * C * (r-μ)/r * γ/(1+γ)
-    )
-    return LelandModel(r, μ, σ, C, τ, Δ, γ, η, δb)
-end
-"""
-Analytical solution to Leland model. Given current cashflow δ, return the equity value.
-"""
-function (leland::LelandModel)(δ)
-    r, μ, C, τ, γ, δb = leland.r, leland.μ, leland.C, leland.τ, leland.γ, leland.δb
-    if δ < δb
-        return zero(δ)
-    else
-        return δ/(r-μ) - (1-τ) * C/r + ((1-τ)*C/r - δb/(r-μ))*(δ/δb)^-γ
-    end
+function LelandModel(; r = 0.02, μ = 0.0, σ = 0.1, C = 0.05, τ = 0.2)
+    return LelandModel(r, μ, σ, C, τ)
 end
 
-
-function initialize_y(leland, grid)
-    r, μ, τ, C = leland.r, leland.μ, leland.τ, leland.C
+function initialize_y(m::LelandModel, grid)
+    r, μ, σ, C, τ = m.r, m.μ, m.σ, m.C, m.τ
     Efunc = δ -> δ/(r-μ) - (1-τ) * C/r
     return OrderedDict(:E => max.(Efunc.(grid[:δ]), 0.0))
 end
 
 
-function (leland::LelandModel)(state::NamedTuple, y::NamedTuple)
-    r, μ, σ, τ, C = leland.r, leland.μ, leland.σ, leland.τ, leland.C
+function (m::LelandModel)(state::NamedTuple, y::NamedTuple)
+    r, μ, σ, C, τ = m.r, m.μ, m.σ, m.C, m.τ
     δ = state.δ
     E, Eδ, Eδδ = y.E, y.Eδ, y.Eδδ
     μδ, σδ = μ * δ, σ * δ
@@ -58,13 +31,24 @@ function (leland::LelandModel)(state::NamedTuple, y::NamedTuple)
 end
 
 le = LelandModel()
-δgrid = [0:0.005:0.2;]
-grid = OrderedDict(:δ => δgrid)
-
+grid = OrderedDict(:δ => [0:0.005:0.2;])
 y0 = initialize_y(le, grid)
-
 y̲ = zeros(length(grid[:δ]))
 y, result, distance = pdesolve(le, grid, y0; y̲ = y̲, bc = OrderedDict(:Eδ => (0.0, 1/(le.r - le.μ),)), reformulation = :smooth)
 
 
-
+"""
+    Analytical solution to Leland model. Given current cashflow δ, return the equity value.
+"""
+function analytical_solution(m::LelandModel, δ)
+    r, μ, σ, C, τ = m.r, m.μ, m.σ, m.C, m.τ
+    Δ = (1/2 * σ^2 - μ)^2 + 2 * σ^2 * r
+    γ = (μ - 1/2 * σ^2 + sqrt(Δ))/σ^2
+    η = -(μ - 1/2 * σ^2 - sqrt(Δ))/σ^2
+    δb = (1-τ) * C * (r-μ)/r * γ/(1+γ)
+    if δ < δb
+        return zero(δ)
+    else
+        return δ/(r-μ) - (1-τ) * C/r + ((1-τ)*C/r - δb/(r-μ))*(δ/δb)^-γ
+    end
+end

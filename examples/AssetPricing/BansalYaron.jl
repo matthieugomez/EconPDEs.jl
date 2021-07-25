4fd8c152a0c7c1fd4589d069e8349bffe5bc16be
matthieugomez <gomez.matthieu@gmail.com>
2021-07-25 12:07:01 -0700

    Update BansalYaron.jl

--- examples/AssetPricing/BansalYaron.jl
+++ examples/AssetPricing/BansalYaron.jl
@@ -53,10 +53,10 @@ end
 m = BansalYaronModel()
 μn = 30
 νn = 30
-distribution = Normal(m.μbar,  sqrt(m.νμ^2 * m.vbar / (2 * m.κμ)))
-μs = range(quantile(distribution, 0.025), quantile(distribution, 0.975), length = μn)
-distribution = Gamma(2 * m.κv * m.vbar / m.νv^2, m.νv^2 / (2 * m.κv))
-vs = range(quantile(distribution, 0.025), quantile(distribution, 0.975), length = νn)
+μdistribution = Normal(m.μbar,  sqrt(m.νμ^2 * m.vbar / (2 * m.κμ)))
+μs = range(quantile(μdistribution, 0.025), quantile(μdistribution, 0.975), length = μn)
+νdistribution = Gamma(2 * m.κv * m.vbar / m.νv^2, m.νv^2 / (2 * m.κv))
+vs = range(quantile(νdistribution, 0.025), νquantile(distribution, 0.975), length = νn)
 stategrid = OrderedDict(:μ => μs, :v => vs)
 yend = OrderedDict(:p => ones(μn, νn))
 y, result, distance = pdesolve(m, stategrid, yend)

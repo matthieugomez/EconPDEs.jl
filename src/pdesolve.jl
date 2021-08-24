ae566d83a58cccc5077108775e112f1328323267
matthieugomez <gomez.matthieu@gmail.com>
2021-08-24 13:34:42 +0200

    Update pdesolve.jl

--- src/pdesolve.jl
+++ src/pdesolve.jl
@@ -9,8 +9,8 @@
        state is a tuple of real numbers of  length S (for the state values)
        y is a tuple of real numbers of length F * 3 with the functions as well as their first and second derivatives at the state
        out is a named tuple of length F with the value of the time derivatives of the functions at the state 
-    * grid is an NamedTuple that, for each state, associates an AbstractVector (for the grid)
-    * yend is an NamedTuple that gives the terminal value of each function in the system of PDEs
+    * grid is an OrderedDict that, for each state, associates an AbstractVector (for the grid)
+    * yend is an OrderedDict that gives the terminal value of each function in the system of PDEs
     * τs (optional) is a time grid on which to solve the pde on. In this case, yend corresponds to the solution at time τs[end]
 """
 function pdesolve(apm, @nospecialize(grid), @nospecialize(yend), τs::Union{Nothing, AbstractVector} = nothing; is_algebraic = OrderedDict(k => false for k in keys(yend)), bc = nothing, verbose = true, kwargs...)

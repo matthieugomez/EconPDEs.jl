74581816d4f8d687c836b56d5a20e61a8fbac057
Matthieu Gomez <gomez.matthieu@gmail.com>
2023-03-06 08:57:50 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,20 +1,20 @@
 using Test
 
 for x in (:CampbellCochrane, :Wachter, :BansalYaron, :GarleanuPanageas, :DiTella, :Haddad, :TuckmanVila)
-	@testset "$x" include("../examples/AssetPricing/$(x).jl") 
+	@testset "$x" begin include("../examples/AssetPricing/$(x).jl") end
 end
 
 for x in (:AchdouHanLasryLionsMoll_Diffusion, :AchdouHanLasryLionsMoll_DiffusionTwoAssets)
-	@testset "$x" include("../examples/ConsumptionProblem/$(x).jl") 
+	@testset "$x" begin include("../examples/ConsumptionProblem/$(x).jl") end
 end
 
 
 for x in (:BoltonChenWang, )
-	@testset "$x" include("../examples/InvestmentProblem/$(x).jl") 
+	@testset "$x" begin include("../examples/InvestmentProblem/$(x).jl") end
 end
 
 for x in (:Leland, )
-	@testset "$x" include("../examples/OptimalStoppingTime/$(x).jl") 
+	@testset "$x" begin include("../examples/OptimalStoppingTime/$(x).jl") end
 end
 
 
